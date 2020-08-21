487a73a05445c887ed2195d2044542025f006962
Garren Souza <contato.garren@gmail.com>
2020-08-21 19:04:51 -0300

    adjustments to local execution

--- vima/compile.sh
+++ vima/compile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-HOME="/home/sairo/Experiment"
+HOME="/home/garren/Documents/Projects/video_simulations"
 SIM_HOME=$HOME"/OrCS"
 PIN_HOME=$SIM_HOME"/trace_generator/pin"
 SINUCA_TRACER_HOME=$SIM_HOME"/trace_generator/extras/pinplay/bin/intel64/sinuca_tracer.so"
 CODE_HOME=$HOME"/orcs_validation/vima"
 COMP_FLAGS="-O2 -DNOINLINE -static"
-SIZES=(1)
-# 2 4 8 16 32 64)
-SIZES_MATMUL=(1 2 4 8)
+SIZES=(4 8)
+SIZES_MATMUL=(4)
+MATMUL_VECTOR_SIZE=(64)
 
 cd $CODE_HOME
 
@@ -23,17 +23,24 @@ for i in *.c
 do 
     rm exec/${i%.c}.out
     g++ $i $COMP_FLAGS -o exec/${i%.c}.out
-    if [[ ${i%.c} == memset* ]]; then
-	for j in "${SIZES[@]}";
-	do
+    echo "Working on "$i
+    # if [[ ${i%.c} == matmul* ]]; then
+    #     for j in "${SIZES_MATMUL[@]}";
+    #     do
+    #         for k in "${MATMUL_VECTOR_SIZE[@]}";
+    #         do
+    #             echo "$PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} ${k} &> nohup.out &"
+    #             #nohup $PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} &> nohup.out &
+    #             $PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} ${k}
+    #         done
+    #     done
+    # else
+    if [[ ${i%.c} == vecsum* ]]; then
+    	for j in "${SIZES[@]}";
+    	do
     		echo "$PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} &> nohup.out &"
-	    	#nohup $PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} &> nohup.out &
-	done
-    #else
-    #	for j in "${SIZES_MATMUL[@]}";
-    #	do
-    #		echo "$PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} &> nohup.out &"
-    #	    	#nohup $PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} &> nohup.out &
-    #	done
+            #nohup $PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} &> nohup.out &
+            $PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j}
+    	done
     fi
 done
--- vima/simulate.sh
+++ vima/simulate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HOME="/home/srsantos/Experiment"
+HOME="/home/garren/Documents/Projects/video_simulations"
 SIM_HOME=$HOME"/OrCS"
 CODE_HOME=$HOME"/orcs_validation/vima"
 TRACE_HOME=$HOME"/orcs_validation/vima/traces"
@@ -7,8 +7,8 @@ DATE_TIME=$(date '+%d%m%Y_%H%M%S');
 
 cd $CODE_HOME
 
-if [ ! -d "resultados" ]; then
-	mkdir -p "resultados"
+if [ ! -d "results" ]; then
+	mkdir -p "results"
 fi
 
 cd $TRACE_HOME
@@ -16,13 +16,13 @@ cd $TRACE_HOME
 for i in *.tid0.stat.out.gz
 do 
     cd $SIM_HOME
-    if [[ ${i%.c} != matmul* && ${i%.c} != *_256* ]]; then
-        TRACE=${i%.tid0.stat.out.gz}
-        echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/sandy_vima_8192.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &"
-        nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/sandy_vima_8192.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &
-    else
-        TRACE=${i%.tid0.stat.out.gz}
-        echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/sandy_vima_256.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &"
-        #nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/sandy_vima_256.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &
-    fi
+    # if [[ ${i%.c} != matmul* && ${i%.c} != *_256* ]]; then
+    TRACE=${i%.tid0.stat.out.gz}
+    echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/sandy_vima_8192.cfg &> ${CODE_HOME}/results/${TRACE}_${DATE_TIME}.txt &"
+    ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/sandy_vima_8192.cfg &> ${CODE_HOME}/results/${TRACE}_${DATE_TIME}.txt
+    # else
+    #     TRACE=${i%.tid0.stat.out.gz}
+    #     echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/sandy_vima_256.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &"
+    #     #nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/sandy_vima_256.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &
+    # fi
 done
